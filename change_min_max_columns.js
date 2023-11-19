0749313e0303cb12ad859dfd37bc8eadc8edeb7d
Герман Алина <151366227+AlinaGerman@users.noreply.github.com>
2023-11-19 15:41:07 +0700

    Update change_min_max_columns.js

--- change_min_max_columns.js
+++ change_min_max_columns.js
@@ -18,14 +18,14 @@ if (isNaN(Number(number_of_lines)) || isNaN(Number(number_of_columns)) || Number
             matrix_output[i]=matrix[i].join('      ');
         }
         alert(matrix_output.join('\n'));
-        var max_elemnent=matrix[0][0];
+        var max_element=matrix[0][0];
         var min_element=matrix[0][0];
         column_max_element=0;
         column_min_element=0;
         for (let i=0;i<matrix.length;i++){
             for (let j=0;j<matrix[i].length;j++){
-                if (matrix[i][j]>max_elemnent){
-                    max_elemnent=matrix[i][j];
+                if (matrix[i][j]>max_element){
+                    max_element=matrix[i][j];
                     column_max_element=j;
                 }else if(matrix[i][j]<min_element){
                     min_element=matrix[i][j];
